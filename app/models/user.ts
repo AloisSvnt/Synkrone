d2fa17f694356f843dcdeb0270c8973d981d683a
AloisSvnt <alois.sauvonnet@live.fr>
2025-06-23 12:03:50 +0200

    feat: add relation in user model

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,11 +1,15 @@
 import { DateTime } from 'luxon'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { DbRememberMeTokensProvider } from '@adonisjs/auth/session'
 
 import { Roles } from '../enum/Role.js'
+
+import Block from '#models/block'
+import SharedBlock from '#models/shared_block'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -35,5 +39,10 @@ export default class User extends compose(BaseModel, AuthFinder) {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
-  
+
+  @hasMany(() => Block)
+  declare blocks: HasMany<typeof Block>
+
+  @hasMany(() => SharedBlock, { foreignKey: 'sharedWithId' })
+  declare sharedBlocks: HasMany<typeof SharedBlock>
 }
