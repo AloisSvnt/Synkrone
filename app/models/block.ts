b2a1896ac777b0e5d87942e8f6cd9594ca886cc3
AloisSvnt <alois.sauvonnet@live.fr>
2025-06-23 11:55:08 +0200

    refactor: app/models/block.ts

--- app/models/block.ts
+++ app/models/block.ts
@@ -1,6 +1,7 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
 import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+
 import User from '#models/user'
 
 export default class Block extends BaseModel {
